6b09f39f02f38e7aa89b2768b286acad2f43e174
Tanshit Rahman <rahmantanshit@gmail.com>
2019-11-05 17:47:00 +0530

    react-github-ui

---
+++ build/precache-manifest.8111cb088606da6d64948299ac388148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e7d3d59be3ff959e8f840bc7d47f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ec7b809e11c3b6f6acb",
+    "url": "/static/css/main.0c61420b.chunk.css"
+  },
+  {
+    "revision": "64819353043f70356d2a",
+    "url": "/static/js/2.ef0c875d.chunk.js"
+  },
+  {
+    "revision": "9ec7b809e11c3b6f6acb",
+    "url": "/static/js/main.7a333efe.chunk.js"
+  },
+  {
+    "revision": "4acfebd908a01daa1fe2",
+    "url": "/static/js/runtime-main.39e85dfe.js"
+  }
+]);
