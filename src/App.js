38e6e3d3cc524c3df41b5cb816d2a8254dc1e2c1
souravkumar93 <soukumar@paypal.com>
2019-11-21 22:52:52 +0530

    data file

--- src/App.js
+++ src/App.js
@@ -2,34 +2,12 @@
 import React, { Component } from 'react'
 import './App.css';
 import Tile from './MemberTile';
-
+import githubData from './data';
 
 class App extends Component {
 
-  state={
-    members:[
-      {
-      url:'https://avatars0.githubusercontent.com/u/10206837?s=400&v=4',
-      repoName:'PayPal_sandbox',
-      owner:'A',
-      forkCount:1,
-      starCount:100,
-      pullRequest:1,
-       followers:8,
-      following:1
-    },
-      {
-      url:'https://avatars2.githubusercontent.com/u/31477944?s=400&u=f8680a0f441849fa1b363af8afe2376cd1f5ef60&v=4',
-      repoName:'PayPal_sdk',
-      owner:'B',
-      forkCount:3,
-      starCount:200,
-      pullRequest:2,
-      followers:1,
-      following:1
-    },
-   
-  ]
+  state = {
+    members: githubData
   }
 
   render() {
@@ -37,31 +15,31 @@ class App extends Component {
 
     return (
       <div className="App">
-      <p>&nbsp;</p>
-      <input type="text" aria-required="true" size="225" value=""
-        name="Search Repo" placeholder="Search Repo" style={{ "width": "1300px", "height": "30px" }} />&nbsp;
-    
-    <button style={{ "width": "100px", "height": "30px" }} >Search</button>
-      <p style={{ "text-align": "center" }}>20 Results Found</p>
-      {
-        members.map(d => <Tile 
-          urlOwner={d.url} 
-          repoName={d.repoName} 
-          owner={d.owner} 
-          forkCount={d.forkCount} 
-          starCount={d.starCount}
-          pullRequest={d.pullRequest}
-          followers={d.followers}
-          following={d.following}
-          />)
-      }
+        <p>&nbsp;</p>
+        <input type="text" aria-required="true" size="225" value=""
+          name="Search Repo" placeholder="Search Repo" style={{ "width": "1300px", "height": "30px" }} />&nbsp;
 
-      <div>
-        <button style={{ "width": "100px", "height": "30px" }} >Prev</button> 
-        Page 2 
+    <button style={{ "width": "100px", "height": "30px" }} >Search</button>
+        <p style={{ "text-align": "center" }}>20 Results Found</p>
+        {
+          members.map(d => <Tile
+            urlOwner={d.avatar_url}
+            repoName={d.full_name}
+            owner={d.login}
+            forkCount={d.forks_count}
+            starCount={d.watchers_count}
+            pullRequest={d.pullRequest}
+            followers={d.followers}
+            following={d.following}
+          />)
+        }
+
+        <div>
+          <button style={{ "width": "100px", "height": "30px" }} >Prev</button>
+          Page 2
         <button style={{ "width": "100px", "height": "30px" }}>Next</button>
+        </div>
       </div>
-</div>
     )
   }
 }
